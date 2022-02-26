c43df253c2d1df0868ad069f209cdd1226eec8b9
David Shen <swotong@davids-mbp.lan>
2022-02-25 22:26:50 -0800

    like

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,8 @@
 
-// import * as admin from "firebase-admin"
+import * as admin from "firebase-admin"
 import {like} from './user/like'
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+admin.initializeApp()
 
 
 exports.like = like
--- functions/src/user/like.ts
+++ functions/src/user/like.ts
@@ -1,18 +1,48 @@
 import * as functions from "firebase-functions"
 import * as admin from "firebase-admin"
 
-
 export const like = functions.https.onCall((data, context) => {
 	const authUser = context.auth
 
-  if (!authUser) {
-		console.error('401')
-    return
+	if (!authUser) {
+		console.error("401")
+		return
 	}
-  const {uid:userId} = authUser
+	const { uid: selfUserId } = authUser
+	const { like, targetUserId } = data
 
-	const userRef = admin.firestore().collection("users").doc(userId)
-  console.log(userRef)
+	const targetUserRef = admin
+		.firestore()
+		.collection("users")
+		.doc(targetUserId)
+	const selfUserContactDocRef = admin
+		.firestore()
+		.collection("users")
+		.doc(selfUserId)
+		.collection("contacts")
+		.doc(targetUserId)
+	const targetUserContactDocRef = admin
+		.firestore()
+		.collection("users")
+		.doc(targetUserId)
+		.collection("contacts")
+		.doc(selfUserId)
 
-  console.log('like============')
+	// IlikeYouTime and youLikeMeTime are also updated when dislike happen
+	// which is ok, we can deduce whether the time is like or dislike time
+	selfUserContactDocRef.set(
+		{ id: targetUserId, IlikeYou: like, IlikeYouTime: new Date() },
+		{ merge: true }
+	)
+	targetUserContactDocRef.set(
+		{ id: selfUserId, youLikeMe: like, youLikeMeTime: new Date() },
+		{ merge: true }
+	)
+	targetUserRef.update({
+		likeCount: admin.firestore.FieldValue.increment(like ? 1 : -1),
+	})
+
+	// TODO: check today's like quota and update it
+
+	return { success: true }
 })
