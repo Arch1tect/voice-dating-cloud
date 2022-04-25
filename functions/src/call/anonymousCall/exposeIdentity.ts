7827c75ee46f8a5ccb3843a4a73b3a96d345f1d6
David Shen <swotong@davids-mbp.lan>
2022-04-25 01:31:14 -0700

    match with anonymous user

--- functions/src/call/anonymousCall/exposeIdentity.ts
+++ functions/src/call/anonymousCall/exposeIdentity.ts
@@ -5,6 +5,52 @@ type callData = {
 	hasExposedIdentity?: boolean
 }
 
+// TBD: important, there is race condition when we implement it this way.
+// An alternative would be for client to check for the state that both self
+// and other user have exposed identity then, call a match endpoint in backend.
+async function matchUsers(selfUserId: string, contactUserId: string) {
+	const createdAt = new Date()
+	const selfUserLikedDocRef = admin
+		.firestore()
+		.collection("users")
+		.doc(selfUserId)
+		.collection("peopleILiked")
+		.doc(contactUserId)
+	const selfUserLikedMeDocRef = admin
+		.firestore()
+		.collection("users")
+		.doc(selfUserId)
+		.collection("peopleLikedMe")
+		.doc(contactUserId)
+	const contactUserLikedDocRef = admin
+		.firestore()
+		.collection("users")
+		.doc(contactUserId)
+		.collection("peopleILiked")
+		.doc(selfUserId)
+	const contactUserLikedMeDocRef = admin
+		.firestore()
+		.collection("users")
+		.doc(contactUserId)
+		.collection("peopleLikedMe")
+		.doc(selfUserId)
+
+	selfUserLikedDocRef.set({ id: contactUserId, createdAt })
+	selfUserLikedMeDocRef.set({ id: contactUserId, createdAt })
+	contactUserLikedDocRef.set({ id: selfUserId, createdAt })
+	contactUserLikedMeDocRef.set({ id: selfUserId, createdAt })
+
+	const selfRef = admin.firestore().collection("users").doc(selfUserId)
+	selfRef.update({
+		likeCount: admin.firestore.FieldValue.increment(1),
+	})
+
+	const contactRef = admin.firestore().collection("users").doc(contactUserId)
+	contactRef.update({
+		likeCount: admin.firestore.FieldValue.increment(1),
+	})
+}
+
 export const exposeIdentity = functions.https.onCall(async (data, context) => {
 	const authUser = context.auth
 
@@ -39,8 +85,8 @@ export const exposeIdentity = functions.https.onCall(async (data, context) => {
 	contactCallRef.update({ contact: selfUser })
 	const contactCallData = (await contactCallRef.get()).data() as callData
 	if (contactCallData?.hasExposedIdentity) {
-		//  TODO: add to match
-		console.log("match")
+		// both has exposed identity, match two users now
+		matchUsers(selfUserId, contactId)
 	}
 	return { success: true }
 })
