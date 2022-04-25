8b8e6f0d91db7abb982df199b4a83ab59db01068
David Shen <swotong@davids-mbp.lan>
2022-04-25 05:24:40 -0700

    Update unlockMessaging.ts

--- functions/src/call/anonymousCall/unlockMessaging.ts
+++ functions/src/call/anonymousCall/unlockMessaging.ts
@@ -5,30 +5,36 @@ import { MessageDataToSaveIntoFirestore } from "../../message/type"
 import { CallData } from "../type"
 
 async function becomeContacts(selfUserId: string, contactId: string) {
+	const createdAt = new Date()
 	const messageId = uuidv4()
 	const messageData: MessageDataToSaveIntoFirestore = {
 		_id: messageId,
 		system: true,
 		text: "Unlocked text messaging",
-		createdAt: new Date(),
+		createdAt,
 	}
 
-	const selfMessageRef = admin
+	const contactRef = admin
 		.firestore()
 		.collection("users")
 		.doc(selfUserId)
 		.collection("contacts")
 		.doc(contactId)
-		.collection("messages")
-		.doc(messageId)
-	const contactMessageRef = admin
+
+	contactRef.set({ id: contactId, createdAt })
+
+	const selfRef = admin
 		.firestore()
 		.collection("users")
 		.doc(contactId)
 		.collection("contacts")
 		.doc(selfUserId)
-		.collection("messages")
-		.doc(messageId)
+
+	selfRef.set({ id: selfUserId, createdAt })
+
+	const selfMessageRef = contactRef.collection("messages").doc(messageId)
+
+	const contactMessageRef = selfRef.collection("messages").doc(messageId)
 
 	selfMessageRef.set(messageData)
 	contactMessageRef.set(messageData)
